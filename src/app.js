d6a8847822dee34d5c99a07eb587f96745292b28
Kairrot <val.callisto@gmail.com>
2021-06-13 07:41:23 +0200

    Change public directory priority 3

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.static(path.join(__dirname, '../client/dist')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use(require('./router'))
 
